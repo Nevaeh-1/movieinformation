fda1268903d6f44ff83dba980620498fc197304e
Nevaeh <a779147171@gmail.com>
2019-09-22 00:01:20 +0800

    typo； 新增提取与储存网页html部分代码； 新增从数据库中读取网页html代码，从中解析与提取必要信息； 新增提取电影海报代码；

--- movie.py
+++ movie.py
@@ -36,38 +36,68 @@ n = requests.Session().get(url).text
 soup = BeautifulSoup(n, "lxml")
 soup = soup.find('div', class_='pages').get_text()
 page = int(soup.split('...')[1])
+
+
+def my_html(page):
+    for x in range(page + 1):
+        if x > 0:
+            print('正在下载第' + str(x) + '页')
+            h = requests.Session().get(url + str(x)).text
+            soup = BeautifulSoup(h, "lxml")
+            session = DBSession()
+            new_moviehtml = moviehtml(id=x, html=str(soup))
+            session.add(new_moviehtml)
+            session.commit()
+        else:
+            continue
+        session.close()
+
+
+print(my_html(20))
+
 m = []
 l = 0
-for x in range(page):
-    print('正在下载第' + str(x) + '页')
-    if x > 0:
-        h = requests.Session().get(url + str(x)).text
-        soup = BeautifulSoup(h, "lxml")
-        soup = soup.find_all('h3', 'f14')
+for id1 in range(21):
+    if id1 > 0:
+        session = DBSession()
+        newhtml = session.query(moviehtml).filter(moviehtml.id == str(id1)).one()
+        newhtml = newhtml.html
+        # print(newhtml)
+        session.close()
+        newsoup = BeautifulSoup(newhtml, 'lxml')
+        newsoup = newsoup.find_all('h3', 'f14')
+        for i in newsoup:
+            # print(i)
+            a = i.get_text('|')
+            aa = i.find('a')
+            w = a.split('|')
+            t = w[0]
+            d = w[1]
+            # print(t)
+            u = 'http://www.zmz2019.com' + aa.get('href')
+            n = requests.Session().get(u).text
+            soup2 = BeautifulSoup(n, "lxml")
+            soup2 = soup2.find('div', class_='imglink')
+            for ii in soup2:
+                print(ii)
+                img = ii.get('href')
+                # print(img)
+            country = d[d.find('【') + 1:d.find('】')]
+            name = d[d.find('《') + 1:d.find('》')]
+            ename = d[d.find('(') + 1:d.find(')')]
+            y = re.search(r'\d{1,4}$', d)
+            if y is not None:
+                y = y.group(0)
+            # print(y)
+            j = [{'type': t, 'country': country, 'ChineseName': name, 'EnglishName': ename, 'year': y, 'image': img}]
+            m.append(j)
+            l = l + 1
+            print('已经爬取第' + str(l) + '个')
+            session = DBSession()
+            new_movie = movie(id=l, type=t, country=country, chinese_name=name, english_name=ename, year=y, url=u, image=img)
+            session.add(new_movie)
+            session.commit()
+        session.close()
     else:
         continue
-    for i in soup:
-        print(i)
-        a = i.get_text('|')
-        aa = i.find('a')
-        w = a.split('|')
-        t = w[0]
-        d = w[1]
-        print(d)
-        u = 'http://www.zmz2019.com' + aa.get('href')
-        country = d[d.find('【') + 1:d.find('】')]
-        name = d[d.find('《') + 1:d.find('》')]
-        ename = d[d.find('(') + 1:d.find(')')]
-        y = re.search(r'\d{1,4}$', d)
-        if y is not None:
-            y = y.group(0)
-        print(y)
-        j = [{'type': t, 'country': country, 'ChineseName': name, 'EnglishName': ename, 'year': y}]
-        m.append(j)
-        l = l + 1
-        session = DBSession()
-        new_movie = movie(id=l, type=t, country=country, chinese_name=name, english_name=ename, year=y, url=u)
-        session.add(new_movie)
-        session.commit()
-    session.close()
 print(json.dumps(m, ensure_ascii=False))
